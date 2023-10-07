238d357fda63150e241aef77bc9dbb01ef43d407
kadiirhocaoglu <kadiirhocaoglu@hotmail.com>
2023-10-07 19:19:06 +0300

    add project settings and color

---
+++ Vakitler/VakitlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VakitlerApp.swift
+//  Vakitler
+//
+//  Created by Kadir Hocaoğlu on 7.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct VakitlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
